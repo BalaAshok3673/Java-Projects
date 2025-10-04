c21d83189b84c4938487b8c22d2a3f286577ee80
bala kumar <balaashok2004@gmail.com>
2025-10-04 13:17:46 +0530

    added Getter and Setter methods to access private fields through methods

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,45 @@
-public class Product {
+abstract class Product {
+    private String name;
+    private int id;
+    private int quantity;
+    private double price;
+
+    public Product(String name, int id , int quantity , double price){
+        this.id=id;
+        this.name=name;
+        this.quantity=quantity;
+        this.price=price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
