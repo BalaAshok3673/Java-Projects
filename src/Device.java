52e1e2524e9202ee9f909729204948b770809e21
Bala Kumar A <balaashok2004@gmail.com>
2025-10-06 12:01:44 +0530

    Created full structure of device class in java

--- src/Device.java
+++ src/Device.java
@@ -1,4 +1,6 @@
-public class Device {
+import java.util.*;
+
+public  abstract class Device {
     private String name;
     private boolean status;
     private double powerConsuption;
@@ -47,4 +49,82 @@ public class Device {
     public abstract void showDetails();
 }
 
+class Light extends Device{
+     private  int brightness;
+
+     public Light(String name , boolean status , double powerConsuption , int brightness){
+         super(name, status, powerConsuption);
+         this.brightness=brightness;
+     }
+
+     @Override
+     public void showDetails(){
+         System.out.println("Light: " + getName() +
+                 ", Brightness: " + brightness +
+                 ", Power Consumption: " + getPowerConsuption() +
+                 ", Status: " + (getStatus() ? "On" : "Off"));
+     }
+}
+
+
+class Fan extends Device{
+    private int speed;
+
+    public Fan(String name, boolean status ,double powerConsuption, int speed){
+         super(name, status, powerConsuption);
+        this.speed=speed;
+
+    }
+
+    @Override
+    public void showDetails(){
+        System.out.println("Fan: " + getName() +
+                ", Speed : " + speed +
+                ", Power Consumption: " + getPowerConsuption() +
+                ", Status: " + (getStatus() ? "On" : "Off"));
+    }
+}
+
+class Ac extends Device{
+    private  int temperature;
+
+    public Ac(String name , boolean status , double powerConsumption , int temperature){
+        super(name , status,powerConsumption);
+        this.temperature=temperature;
+    }
+
+    @Override
+    public void showDetails(){
+        System.out.println("AC: " + getName() +
+                ", Temperature : " + temperature +
+                ", Power Consumption: " + getPowerConsuption() +
+                ", Status: " + (getStatus() ? "On" : "Off"));
+    }
+}
+
+class HomeManager{
+
+    ArrayList<Device> devices;
+
+
+    public HomeManager(){
+        devices = new ArrayList<>();
+    }
+
+   public void addDevice(Device d) {
+       devices.add(d);
+       System.out.println(d.getName() + " added to Home.");
+   }
+
+   public void removeDevice(Device d){
+        devices.remove(d);
+       System.out.println(d.getName() + " removed from Home");
+   }
+
+   public void  showAllDevices(){
+        for (Device d : devices){
+            d.showDetails();
+        }
+   }
+}
 
